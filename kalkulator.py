8b5cb1fb0298c74023b708fc52bf00e9d3cbb349
mkozykowski <mkozykowski@gmail.com>
2016-04-25 18:42:30 +0200

    Mój komentarz2

---
+++ kalkulator.py
@@ -0,0 +1,17 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      mkozykowski
+#
+# Created:     25.04.2016
+# Copyright:   (c) mkozykowski 2016
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def kalkulator():
+    print("kalkulator")
+
+kalkulator()
+
+
